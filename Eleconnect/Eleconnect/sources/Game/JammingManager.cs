b21c17443ec020577a4811c035d11c96e4b91f68
nature5884 <nkc.shuntaro.kobayashi@gmail.com>
2014-05-29 18:35:21 +0900

    ジャミング修正

--- Eleconnect/Eleconnect/sources/Game/JammingManager.cs
+++ Eleconnect/Eleconnect/sources/Game/JammingManager.cs
@@ -6,9 +6,10 @@ namespace Eleconnect
 	public class JammingManager
 	{
 		// ジャミング
-		public int[,] jammingData;							// ジャミング配置データ
-		int jammingX;										// ジャミング最大数(横)
-		int jammingY;										// ジャミング最大数(縦)
+		public int[,] jammingDataLength;						// ジャミング配置データ(縦)
+		public int[,] jammingDataSide;							// ジャミング配置データ(横)
+		int jammingLength;										// ジャミング最大数(横)
+		int jammingSide;										// ジャミング最大数(縦)
 		float panelSize;
 		Vector2 basePos;
 		public Vector3 jammingLocate;
@@ -26,12 +27,13 @@ namespace Eleconnect
 		private void Init()
 		{
 			jamming = new Jamming();
-			jammingX = GameScene.stageWidth-1;
-			jammingY = GameScene.stageHeight*2-1;
+			jammingSide = GameScene.stageWidth;
+			jammingLength = GameScene.stageHeight;
 			panelSize = Panel.SIZE * Panel.SCALE + 5.0f;
 			basePos = new Vector2(AppMain.ScreenCenter.X - ((panelSize * GameScene.stageWidth) / 2.0f) + panelSize,
 			                              AppMain.ScreenCenter.Y - ((panelSize * GameScene.stageHeight) / 2.0f) + panelSize);
-			jammingData = new int[jammingX,jammingY];
+			jammingDataLength = new int[jammingSide,jammingLength];
+			jammingDataSide = new int[jammingSide,jammingLength];
 			jammingLocate = new Vector3(basePos.X + panelSize,
 			                            basePos.Y + panelSize,
 			                            0.0f);
@@ -41,17 +43,18 @@ namespace Eleconnect
 		public void JammingSet()
 		{
 			// ジャミング配置場所の数値は1
-			for(int i = 0;i < jammingX;i++)
+			for(int i = 0;i < jammingSide;i++)
 			{
-				for(int j = 0;j < jammingY;j++)
+				for(int j = 0;j < jammingLength;j++)
 				{
-					//jammingData[i,j] = 1;
+					jammingDataSide[i,j] = 1;
+					jammingDataLength[i,j] = 1;
 					
-					if(i % 2 == 0)
+					/*if(i % 2 == 0)
 					{
 						jammingData[i,j] = 1;
 					}
-					
+					*/
 				}
 			}
 		}
@@ -66,33 +69,36 @@ namespace Eleconnect
 		{
 			if(!JammingSwitch.isJamming) return;
 			
-			for(int i = 0;i < jammingX;i++)
+			// ジャミング縦
+			for(int i = 0;i < jammingSide-1;i++)
 			{
-				for(int j = 0;j < jammingY;j++)
+				for(int j = 0;j < jammingLength;j++)
 				{
-					if(jammingData[i,j] == 1)
+					if(jammingDataLength[i,j] == 1)
 					{
-						// 縦向き
-						if(j%2 == 0)
-						{
-							jammingLocate = new Vector3(basePos.X + panelSize * i,
-				                            		basePos.Y-panelSize/2 + panelSize * (j/2),
+						jammingLocate = new Vector3(basePos.X + panelSize * i,
+				                            		basePos.Y-panelSize/2 + panelSize * j,
 				                            		0.0f);
-							jamming.Draw(jammingLocate,0.0f);
-						}
-						
-						// 横向き
-						if(j%2 == 1)
-						{ 
-							jammingLocate = new Vector3(basePos.X-panelSize/2 + panelSize * i,
-				                            		basePos.Y + panelSize * (j/2),
-				                            		0.0f);
-							jamming.Draw(jammingLocate,changeAngle);
-						}
+						jamming.Draw(jammingLocate,0.0f);
 					}
 				}
 			}
+			
+			// ジャミング横
+			for(int i = 0;i < jammingSide;i++)
+			{
+				for(int j = 0;j < jammingLength-1;j++)
+				{
+					if(jammingDataSide[i,j] == 1)
+					{
+						jammingLocate = new Vector3(basePos.X-panelSize/2 + panelSize * i,
+				                            		basePos.Y + panelSize * j,
+				                            		0.0f);
+						jamming.Draw(jammingLocate,changeAngle);
+					}
+				}
 			}
+		}
 		
 		// 解放
 		public void Term()
