11f8549cd3c3c5ae03cc553857097b1484698ad9
Shion_Kubota <nkc.shion.kubota@gmail.com>
2014-06-20 17:01:31 +0900

    エディットモードアップデート(ステージサイズ変更可に)。メモリ管理を改善。

---
+++ Eleconnect/Eleconnect/sources/Game/Stage/Stage_4.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Eleconnect
+{
+	public class Stage_4
+	{
+		public Stage_4 ()
+		{
+		}
+	}
+}
+
---
+++ Eleconnect/Eleconnect/sources/Game/Stage/Stage_5.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Eleconnect
+{
+	public class Stage_5
+	{
+		public Stage_5 ()
+		{
+		}
+	}
+}
+
